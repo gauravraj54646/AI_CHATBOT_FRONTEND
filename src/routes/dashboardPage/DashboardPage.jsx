64daebd1c1cff05ca08fb9a41b66f19f28d0671d
gauravraj54646 <gauravraj54646@gmail.com>
2024-09-01 08:43:25 +0530

    commit

--- src/routes/dashboardPage/DashboardPage.jsx
+++ src/routes/dashboardPage/DashboardPage.jsx
@@ -9,7 +9,8 @@ const DashboardPage = () => {
 
   const mutation = useMutation({
     mutationFn: async (text) => {
-      return fetch(`${import.meta.env.VITE_API_URL}/api/chats`, {
+      // return fetch(`${import.meta.env.VITE_API_URL}/api/chats`, {
+        return fetch(`/api/chats`, {
         method: "POST",
         credentials: "include",
         headers: {
